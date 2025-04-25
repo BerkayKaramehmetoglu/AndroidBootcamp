3a53e50227149d43f066d014c6ff6e3ebfda1542
Berkay Karamehmetoglu <berkaykaramehmetoglu54@gmail.com>
2025-04-25 14:50:51 +0300

    Bootcamp 1. Ödev Değişken Tanımlama

---
+++ app/src/main/java/com/example/androidbootcamp/odev1/Odev1.kt
@@ -0,0 +1,51 @@
+package com.example.androidbootcamp.odev1
+
+import android.view.WindowInsetsAnimation.Bounds
+import java.sql.Time
+
+val sehir: String = "SAKARYA"
+val odemeTarihi: String = "25/04/2025"
+val marka: String = "Vakko"
+val ulusalGun: String = "1 Mayıs"
+val ulke: String = "Türkiye"
+val odeme: Int = 120000
+val muzikAdi: String = "Hesretin Negmesi"
+val tatilGunu: String = "26/04/2025"
+val telefon: Long = 5305004000
+val siparisAdeti: Byte = 50
+val postaKodu: Int = 54100
+val arabaModeli: Short = 2018
+val videoSuresi: Short = 10
+val rezervasyonTarihi: String = "28/04/2025"
+val email: String = "berkaykaramehmetoglu54@gmail.com"
+val urunPuani: Double = 10.4
+val meslek: String = "Software Developer"
+val kitapAdi: String = "Don Kişot"
+val resimAdi: String = "Mona Lisa"
+val sokakAdi: String = "813 No Sok."
+val stokMiktari: Byte = 10
+val müsteriAdi: String = "Metin"
+val yayinlamaTarihi: String = "1/05/2030"
+val indirimMiktari: Double = 95.5
+val dosyaFormati: String = ".csv"
+val otobusHatti: String = "SAKARYA PARK - KÜPÇÜLER - KUZEY TERMİNAL"
+val bakiye: Short = 100
+val odaSayisi: Byte = 3
+val renk: String = "Beyaz"
+val kalanDakika: Short = 5
+val dogumGunu: String = "07/03"
+val enlem: String = "36–42° Kuzey enlemleri"
+val renkKodu: String = "#931010"
+val takipKodu: Int = 563204
+val maas: Int = 45000
+val boylam: String = "26–45° Doğu boylamları"
+val telefonModeli: String = "15 Pro"
+val kuponSuresi: String = "04/05/2025"
+val medeniDurum: String = "Bekar"
+val urunAdi: String = "Bilgisayar"
+val ekranBoyutu: Double = 15.6
+val kuponKodu: String = "Sepette100"
+val urunYorum: String = "Mükemmel"
+val yemekFiyati: Byte = 50
+val agirlik: Short = 106
+val faturaAdresi: String = "Sakarya/Adapazarı"
